ee9806319e6ff8f5516f1c3d46e0871eb56ec8c4
nitianashu <nitashu7@gmail.com>
2026-04-21 16:05:10 +0530

    Fixed NotificationBell import

--- Frontend/src/components/pages/ActiveOrders/ActiveOrders.jsx
+++ Frontend/src/components/pages/ActiveOrders/ActiveOrders.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { API } from "../../../api/api";
 import { useNavigate } from "react-router-dom";
 import { socket } from "../../../socket";
-import NotificationBell from "../../components/NotificationBell";
+import NotificationBell from "../../NotificationBell";;
 
 function ActiveOrders() {
   const [orders, setOrders] = useState([]);
